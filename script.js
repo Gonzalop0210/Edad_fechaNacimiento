b418a9ecd805f478bc6961ee8c7aed51f0f4616e
Gonzalo Palma <gonzalopalmadev0210@gmail.com>
2023-08-18 17:01:07 -0500

    empezamos con el js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const calendario = document.querySelector("#id_calendar");
-const boton = document.querySelector("id_botonEdad");
+const boton = document.querySelector("#id_botonEdad");
 const mostrar = document.querySelector(".background_respuesta");
+
+boton.addEventListener("click", function() {
+    const fecha = new Date(calendario.value);
+    const dia = fecha.getDay();
+    const mes = fecha.getMonth();
+    const anio = fecha.getFullYear();
+
+    console.log(dia);
+    console.log(mes);
+    console.log(anio);
+});
